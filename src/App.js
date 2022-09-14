b3bfe09bc762e3836faa954f5b503d6a92e3857e
Joy Paul <joypaul3592@gmail.com>
2022-09-14 20:22:15 +0600

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Calculator from './Component.js/Calculator';
 function App() {
   return (
     <div className="App flex flex-col items-center justify-center bg-gray-100 h-screen">
+      <h1>Calculator</h1>
       <Calculator />
     </div>
   );
